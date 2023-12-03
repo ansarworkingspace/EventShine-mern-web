cac397d09a0fb730821c16f053da65ced11d36ac
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-12-03 07:45:22 +0530

    Admin Redex setup done

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './Slice/User/ApiSlice';
 import authReducer from './Slice/User/AuthSlice';
+import adminReducer from './Slice/Admin/AuthSlice'
+
 
 //REDUX STORE SETTINGS
+// const store = configureStore({
+//     reducer: {
+//       [apiSlice.reducerPath]: apiSlice.reducer,
+//       auth: authReducer, // add this line
+//     },
+//     middleware: (getDefaultMiddleware) =>
+//       getDefaultMiddleware().concat(apiSlice.middleware),
+//     devTools: true,
+//   });
+  
+//   export default store;
+
+
+const rootReducer = combineReducers({
+  auth:authReducer,
+  adminAuth:adminReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+
+
 const store = configureStore({
-    reducer: {
-      [apiSlice.reducerPath]: apiSlice.reducer,
-      auth: authReducer, // add this line
-    },
-    middleware: (getDefaultMiddleware) =>
+    reducer: rootReducer,
+      middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
   });
-  
-  export default store;
+
+export default store;
