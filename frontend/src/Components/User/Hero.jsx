ce43a80c3c611c3a8118eb57573df4f5e91c1b92
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-12-04 08:10:19 +0530

    completed

--- frontend/src/Components/User/Hero.jsx
+++ frontend/src/Components/User/Hero.jsx
@@ -1,24 +1,28 @@
 import { Container, Card, Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
 
 const Hero = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+
   return (
     <div className=' py-5'>
       <Container className='d-flex justify-content-center'>
-        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
-          <h1 className='text-center mb-4'>MERN Authentication</h1>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75' style={{border:"dotted",borderColor:"gray",borderRadius:"2rem"}}>
+        <h1 className='text-center mb-4'>
+            {userInfo ? `Welcome ${userInfo.name} 👋` : 'Welcome to Event Shine 👋'}
+          </h1>
           <p className='text-center mb-4'>
-            This is a boilerplate for MERN authentication that stores a JWT in
-            an HTTP-Only cookie. It also uses Redux Toolkit and the React
-            Bootstrap library
+          Dive into the pulse of live moments with our event streaming platform, connecting enthusiasts worldwide in real-time exhilaration.
           </p>
-          <div className='d-flex'>
+          {/* <div className='d-flex'>
             <Button variant='primary' href='/login' className='me-3'>
               Sign In
             </Button>
             <Button variant='secondary' href='/register'>
               Register
             </Button>
-          </div>
+          </div> */}
         </Card>
       </Container>
     </div>
